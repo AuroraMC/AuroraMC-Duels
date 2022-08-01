5e2c4314c95cfc9d76bd7c7a0f4b156df4876181
Block2Block <accounts@block2block.me>
2022-08-01 21:54:43 +0100

    Remove debug messages

--- src/main/java/net/auroramc/duels/utils/damage/StandardDeathListener.java
+++ src/main/java/net/auroramc/duels/utils/damage/StandardDeathListener.java
@@ -6,7 +6,6 @@ import net.auroramc.core.api.cosmetics.DeathEffect;
 import net.auroramc.core.api.cosmetics.KillMessage;
 import net.auroramc.duels.api.AuroraMCDuelsPlayer;
 import net.auroramc.duels.api.game.Game;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.*;
@@ -36,7 +35,6 @@ public class StandardDeathListener implements Listener {
             AuroraMCDuelsPlayer player = (AuroraMCDuelsPlayer) AuroraMCAPI.getPlayer((Player) e.getEntity());
             if (player.isInGame() && games.contains(player.getGame())) {
                 if (e.getFinalDamage() >= ((Player) e.getEntity()).getHealth()) {
-                    Bukkit.broadcastMessage("1");
                     e.setDamage(0);
 
                     Entity entity = null;
@@ -98,7 +96,6 @@ public class StandardDeathListener implements Listener {
                             killReason = KillMessage.KillReason.ENTITY;
                         }
                     } else {
-                        Bukkit.broadcastMessage("2");
                         switch (e.getCause()) {
                             case FALL: {
                                 killReason = KillMessage.KillReason.FALL;
@@ -141,7 +138,6 @@ public class StandardDeathListener implements Listener {
                         }
                     }
                     if (killer != null) {
-                        Bukkit.broadcastMessage("3");
                         if (killer.getActiveCosmetics().containsKey(Cosmetic.CosmeticType.KILL_MESSAGE)) {
                             killMessage = (KillMessage) killer.getActiveCosmetics().get(Cosmetic.CosmeticType.KILL_MESSAGE);
                         } else {
@@ -176,10 +172,8 @@ public class StandardDeathListener implements Listener {
                         ((DeathEffect)player.getActiveCosmetics().get(Cosmetic.CosmeticType.DEATH_EFFECT)).onDeath(player);
                     }
 
-                    Bukkit.broadcastMessage("4");
 
                     player.getGame().onDeath(player);
-                    Bukkit.broadcastMessage("5");
                 } else {
                     if (e instanceof EntityDamageByEntityEvent) {
                         if (((EntityDamageByEntityEvent) e).getDamager().equals(e.getEntity())) {
