1802fc797fc0a83973faee45178c057caf64bb74
Block2Block <accounts@block2block.me>
2022-08-02 18:09:00 +0100

    Add borders

--- src/main/java/net/auroramc/duels/listeners/DisableMoveListener.java
+++ src/main/java/net/auroramc/duels/listeners/DisableMoveListener.java
@@ -1,11 +1,15 @@
 package net.auroramc.duels.listeners;
 
 import net.auroramc.core.api.AuroraMCAPI;
+import net.auroramc.core.api.players.AuroraMCPlayer;
 import net.auroramc.duels.api.AuroraMCDuelsPlayer;
 import net.auroramc.duels.api.game.Game;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.json.JSONObject;
 
 public class DisableMoveListener implements Listener {
 
@@ -16,6 +20,48 @@ public class DisableMoveListener implements Listener {
             if (duelsPlayer.isInGame() && duelsPlayer.getGame().getGameState() == Game.GameState.STARTING) {
                 if (e.getFrom().getBlockX() != e.getTo().getBlockX() || e.getFrom().getBlockZ() != e.getTo().getBlockZ()) {
                     e.setTo(e.getFrom());
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBorder(PlayerMoveEvent e) {
+        if (!e.getFrom().getBlock().getLocation().equals(e.getTo().getBlock().getLocation())) {
+            AuroraMCPlayer player = AuroraMCAPI.getPlayer(e.getPlayer());
+            AuroraMCDuelsPlayer dp = (AuroraMCDuelsPlayer) player;
+            if (dp.isInGame()) {
+                int highX = 0, lowX = 0, highY = 0, lowY = 0, highZ = 0, lowZ = 0;
+                JSONObject a = dp.getGame().getMap().getMapData().getJSONObject("border_a");
+                JSONObject b = dp.getGame().getMap().getMapData().getJSONObject("border_b");
+                if (a.getInt("x") > b.getInt("x")) {
+                    highX = a.getInt("x");
+                    lowX = b.getInt("x");
+                } else {
+                    highX = b.getInt("x");
+                    lowX = a.getInt("x");
+                }
+
+                if (a.getInt("y") > b.getInt("y")) {
+                    highY = a.getInt("y");
+                    lowY = b.getInt("y");
+                } else {
+                    highY = b.getInt("y");
+                    lowY = a.getInt("y");
+                }
+
+                if (a.getInt("z") > b.getInt("z")) {
+                    highZ = a.getInt("z");
+                    lowZ = b.getInt("z");
+                } else {
+                    highZ = b.getInt("z");
+                    lowZ = a.getInt("z");
+                }
+
+                if (e.getTo().getX() < lowX || e.getTo().getX() > highX || e.getTo().getY() < lowY || e.getTo().getY() > highY || e.getTo().getZ() < lowZ || e.getTo().getZ() > highZ) {
+                    //Call entity damage event so the games can handle them appropriately.
+                    EntityDamageEvent event = new EntityDamageEvent(e.getPlayer(), EntityDamageEvent.DamageCause.VOID, 500);
+                    Bukkit.getPluginManager().callEvent(event);
                 }
             }
         }
